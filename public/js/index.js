511acc81160a85e04d9880dfec76a7914a434f85
Kkura-chan <thienkiempro1@gmail.com>
2022-12-05 16:10:20 +0700

    update UI

--- public/js/index.js
+++ public/js/index.js
@@ -17,25 +17,49 @@ search_form.addEventListener("submit", async function (e) {
     details.remove();
   }
   if (data.message) {
-    const html = `<div class="details"><span class="err-msg">${data.message}</span></div>`;
+    const html = `<div class="details err"><span class="err-msg">${data.message}</span></div>`;
     search_form.insertAdjacentHTML("afterEnd", html);
     search_input.value = "";
     return;
   }
-  const html = `<div class="details">
-  <div class="date">${new Intl.DateTimeFormat(data.sys.country, {
-    dateStyle: "full",
-    timeStyle: "long",
-  }).format(data.dt * 1000)}</div>
-  <div class="place">${data.name}-${data.sys.country}</div>
-  <div class="temp">${data.main.temp}°C</div>
-  <div class="humidity">Độ ẩm ${data.main.humidity}%</div>
-  <div class="description" style="display:flex">${
-    data.weather[0].description
-  }</div>
-  <img src="../img/${data.weather[0].icon}@2x.png"/>
-    </div>`;
+  const html = `
+<div class="details">
+  <div class="top">
+    <div>
+      <p class="date">${new Intl.DateTimeFormat(data.sys.country, {
+        dateStyle: "full",
+        timeStyle: "medium",
+      }).format(data.dt * 1000)}</p>
+      <p class="place">${data.name}-${data.sys.country}</p>
+      <p class="description">${data.weather[0].description}</p>
+    </div>
+    <img alt="weather" class="weather-icon" src="../img/${
+      data.weather[0].icon
+    }@2x.png"/>
+  </div>
+  <div class="bottom">
+    <p class="temp">${data.main.temp}°C</p>
+    <div class="more-details">
+      <div class="parameter-row">
+        <span class="parameter-label">Độ ẩm</span>
+        <span class="parameter-value">${data.main.humidity}%</span>
+      </div>
+      <div class="parameter-row">
+        <span class="parameter-label">Tầm nhìn xa trên</span>
+        <span class="parameter-value">${data.visibility / 1000} Km</span>
+      </div>
+      <div class="parameter-row">
+        <span class="parameter-label">Nhiệt độ cao nhất</span>
+        <span class="parameter-value">${data.main.temp_max}°C</span>
+      </div>
+      <div class="parameter-row">
+        <span class="parameter-label">Nhiệt độ thấp nhất</span>
+        <span class="parameter-value">${data.main.temp_min}</span>
+      </div>
+    </div>
+  </div>
 
+</div>`;
   search_form.insertAdjacentHTML("afterEnd", html);
   search_input.value = "";
 });
--- public/js/map.js
+++ public/js/map.js
@@ -26,19 +26,42 @@ if (navigator.geolocation) {
         details.remove();
       }
       const html = `<div class="details">
-      <div class="date">${new Intl.DateTimeFormat(data.sys.country, {
-        dateStyle: "full",
-        timeStyle: "long",
-      }).format(data.dt * 1000)}</div>
-      <div class="place">${data.name}-${data.sys.country}</div>
-      <div class="temp">${data.main.temp}°C</div>
-      <div class="humidity">Độ ẩm ${data.main.humidity}%</div>
-      <div class="description" style="display:flex">${
-        data.weather[0].description
-      } <img src="../img/${
-        data.weather[0].icon
-      }@2x.png" style="width:100px" /></div>
-        </div>`;
+      <div class="top">
+        <div>
+          <p class="date">${new Intl.DateTimeFormat(data.sys.country, {
+            dateStyle: "full",
+            timeStyle: "medium",
+          }).format(data.dt * 1000)}</p>
+          <p class="place">${data.name}-${data.sys.country}</p>
+          <p class="description">${data.weather[0].description}</p>
+        </div>
+        <img alt="weather" class="weather-icon" src="../img/${
+          data.weather[0].icon
+        }@2x.png"/>
+      </div>
+      <div class="bottom">
+        <p class="temp">${data.main.temp}°C</p>
+        <div class="more-details">
+          <div class="parameter-row">
+            <span class="parameter-label">Độ ẩm</span>
+            <span class="parameter-value">${data.main.humidity}%</span>
+          </div>
+          <div class="parameter-row">
+        <span class="parameter-label">Tầm nhìn xa trên</span>
+        <span class="parameter-value">${data.visibility / 1000} Km</span>
+          </div>
+          <div class="parameter-row">
+          <span class="parameter-label">Nhiệt độ cao nhất</span>
+          <span class="parameter-value">${data.main.temp_max}°C</span>
+          </div>
+          <div class="parameter-row">
+          <span class="parameter-label">Nhiệt độ thấp nhất</span>
+          <span class="parameter-value">${data.main.temp_min}°C</span>
+          </div>
+        </div>
+      </div>
+    
+    </div>`;
       home_page.insertAdjacentHTML("afterEnd", html);
     });
   });
